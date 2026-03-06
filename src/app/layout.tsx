cec02a631fec5585d2b49c0876aa6e8a037dc73c
Sefa Ocak <sefa.ocak@tenettechnology.com.tr>
2026-03-06 17:05:47 +0300

    Mantine CSS import eksikligi giderildi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { MantineProvider, ColorSchemeScript, createTheme } from '@mantine/core';
+import '@mantine/core/styles.css';
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
 import "./globals.css";
